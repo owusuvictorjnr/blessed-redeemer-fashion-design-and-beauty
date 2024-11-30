478854521f7e503e03664192c9d7e8fa78f4c1c6
owusuvictorjnr <owusuvictor80@gmail.com>
2024-11-30 07:07:14 +0000

    home page

---
+++ app/(frontend)/page.tsx
@@ -0,0 +1,16 @@
+import Container from "@/components/Container";
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+  return (
+    <div className="">
+      <Container>
+        <h1>hello</h1>
+
+        <Button variant={"secondary"}>Click me</Button>
+
+        
+      </Container>
+    </div>
+  );
+}
